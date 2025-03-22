cce6ace2d316adf88c8aa5e41f0372fc7ba8e642
Arpan Bhavsar <arpanbhavsar.160410116019@gmail.com>
2025-03-22 10:25:55 -0700

    Fixed NavDrawer navigation and journal_entry_screen.dart navigation to load the list again.

--- lib/screens/journal_entry_screen.dart
+++ lib/screens/journal_entry_screen.dart
@@ -4,6 +4,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:myapp/screens/journal_list_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../services/api_service.dart';
@@ -16,10 +17,8 @@ class JournalEntryScreen extends StatefulWidget {
 }
 
 class _JournalEntryScreenState extends State<JournalEntryScreen> {
-  final TextEditingController _bodyTextEditingController =
-      TextEditingController();
-  final TextEditingController _titleTextEditingController =
-      TextEditingController();
+  final TextEditingController _bodyTextEditingController = TextEditingController();
+  final TextEditingController _titleTextEditingController = TextEditingController();
   late final String userId;
   bool _isLoading = false;
 
@@ -45,32 +44,28 @@ class _JournalEntryScreenState extends State<JournalEntryScreen> {
     final title = _titleTextEditingController.text;
     final body = _bodyTextEditingController.text;
     if (title.isEmpty || body.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Please enter a title and body')),
-      );
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Please enter a title and body')));
       return;
     }
     setState(() {
       _isLoading = true;
     });
-    var apiResponse = await ApiService.post('journal', {
-      'userId': userId,
-      'title': title,
-      'journal_entry': body,
-    });
+    var apiResponse = await ApiService.post('journal', {'userId': userId, 'title': title, 'journal_entry': body});
     if (apiResponse.statusCode >= 200 && apiResponse.statusCode < 300) {
       setState(() {
-      _isLoading = false;
-    });
-      Navigator.pop(context);
+        _isLoading = false;
+      });
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const JournalListScreen()),
+        (Route<dynamic> route) => false, // This removes all previous routes
+      );
     } else {
       final responseData = jsonDecode(apiResponse.body);
       setState(() {
-      _isLoading = false;
-    });
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text(responseData["message"])));
+        _isLoading = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(responseData["message"])));
     }
   }
 
@@ -87,32 +82,29 @@ class _JournalEntryScreenState extends State<JournalEntryScreen> {
           ),
         ],
       ),
-      body: _isLoading ? const Center(child: CircularProgressIndicator(),):Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleTextEditingController,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-              decoration: const InputDecoration(
-                hintText: 'Title',
-                border: InputBorder.none,
-              ),
-            ),
-            Expanded(
-              child: TextField(
-                controller: _bodyTextEditingController,
-                maxLines: null,
-                expands: true,
-                decoration: const InputDecoration(
-                  hintText: 'Start writing your journal entry...',
-                  border: InputBorder.none,
+      body:
+          _isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleTextEditingController,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                      decoration: const InputDecoration(hintText: 'Title', border: InputBorder.none),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: _bodyTextEditingController,
+                        maxLines: null,
+                        expands: true,
+                        decoration: const InputDecoration(hintText: 'Start writing your journal entry...', border: InputBorder.none),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
